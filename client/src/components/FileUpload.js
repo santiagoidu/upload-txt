e9f96dd703559d0871481763b735e9a5b50e280c
Marco Tulio Santiago <48780041+santiagoidu@users.noreply.github.com>
2023-04-03 16:36:40 -0300

    bug: listagem txt some em pouco tempo

--- client/src/components/FileUpload.js
+++ client/src/components/FileUpload.js
@@ -38,10 +38,49 @@ const FileUpload = () => {
       setUploadedFile({ fileName, filePath });
       setMessage("File uploaded");
 
-      // Read file content and store it in the state
+      // Read file content and store it in the state and localStorage
       const reader = new FileReader();
       reader.onload = () => {
-        setFileContent(reader.result);
+        const content = reader.result;
+        setFileContent(content);
+        localStorage.setItem("fileContent", content);
+        // Render the file data into a table
+        const lines = content.split("\n");
+        const data = lines.map((line) => {
+          const columns = line.split(/\s+/);
+          return {
+            data: columns[0],
+            curso: columns[1],
+            cpf: columns[2],
+            nome: columns[3],
+          };
+        });
+        const table = document.createElement("table");
+        const thead = document.createElement("thead");
+        const tbody = document.createElement("tbody");
+        const tr = document.createElement("tr");
+        const headers = ["Data", "Curso", "CPF", "Nome"];
+        headers.forEach((header) => {
+          const th = document.createElement("th");
+          th.textContent = header;
+          tr.appendChild(th);
+        });
+        thead.appendChild(tr);
+        data.forEach((row) => {
+          const tr = document.createElement("tr");
+          const columns = ["data", "curso", "cpf", "nome"];
+          columns.forEach((column) => {
+            const td = document.createElement("td");
+            td.textContent = row[column];
+            tr.appendChild(td);
+          });
+          tbody.appendChild(tr);
+        });
+        table.appendChild(thead);
+        table.appendChild(tbody);
+        const container = document.createElement("div");
+        container.appendChild(table);
+        document.body.appendChild(container);
       };
       reader.readAsText(file);
     } catch (err) {
@@ -81,7 +120,32 @@ const FileUpload = () => {
         <div className="row mt-5">
           <div className="col-md-6 m-auto">
             <h3 className="text-center">{uploadedFile.fileName}</h3>
-            <p style={{ whiteSpace: "pre-wrap" }}>{fileContent}</p>
+            <table className="table table-striped">
+              <thead>
+                <tr>
+                  <th>Data</th>
+                  <th>Curso</th>
+                  <th>CPF</th>
+                  <th>Nome</th>
+                </tr>
+              </thead>
+              <tbody>
+                {fileContent.split("\n").map((line, index) => {
+                  if (line) {
+                    const [data, curso, cpf, nome] = line.split(/[\t]+/);
+                    return (
+                      <tr key={index}>
+                        <td>{data}</td>
+                        <td>{curso}</td>
+                        <td>{cpf}</td>
+                        <td>{nome}</td>
+                      </tr>
+                    );
+                  }
+                  return null;
+                })}
+              </tbody>
+            </table>
             <a
               href={uploadedFile.filePath}
               target="_blank"
@@ -89,9 +153,8 @@ const FileUpload = () => {
             >
               Link para download
             </a>
-            <button onClick={() => window.open(uploadedFile.filePath, "_blank")}>
-              Open in new tab
-            </button>
+
+            <button onClick={() => window.open(uploadedFile.filePath)}>Open File</button>
           </div>
         </div>
       ) : null}
